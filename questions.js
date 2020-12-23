b088e2fa859f51a68af95fa4da88813ed2f41f37
Matt Duffield <duffieldml@gmail.com>
2020-12-22 19:19:28 -0500

    Working on questions

---
+++ questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        title: "What HTML element will contain the Javascript?",
+        choices: ["<Head>", "<Footer>", "<script>", "<div>"],
+        answer: "<script>"
+    },
+    {
+        title: "What do you use to create a pop up box?",
+        choices: ["alert", "console.log", "var", "function"],
+        answer: "alert"
+    },
+    {
+        title: "If you want to create a loop, what do you use?",
+        choices: ["For", "event listener", "query selector", "var"],
+        answer: "For"
+    },
+]
