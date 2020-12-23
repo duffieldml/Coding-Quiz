3454a202cb0b5ab28d4d28f1866c95d1dac8a38c
Matt Duffield <duffieldml@gmail.com>
2020-12-22 21:03:35 -0500

    Working on stopTest function

--- script.js
+++ script.js
@@ -5,20 +5,15 @@
 var timeEl = document.querySelector("#timer");
 var startButton = document.querySelector("#start-quiz");
 var startEl = document.querySelector("#start");
-var quizEl  =   document.querySelector("#quiz");
+var quizEl  = document.querySelector("#quiz");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var resultEl = document.querySelector("#result");
 
 var userScore = 0;
 var secondsLeft = 75;
 var wrongTime = 10;
-var questionIndex = 0;
-var questionsEl = document.querySelector("#questions");
-var choicesEl = document.querySelector("#choices");
-
-
-// for (var i = 0; i < questions; i++) {
-//     var question = passwordChoices[Math.floor(Math.random() * passwordChoices.length)];
-//     questionHolder.push(Choices);
-//   }
+var askQuestionIndex = 0;
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -30,12 +25,64 @@ function setTime() {
       stopTest();
     }
   }, 1000);
-
 };
 
 function subtractTime () {
     secondsLeft = secondsLeft - wrongTime;
+    if (secondsLeft < 0) {
+        secondsLeft = 0;
+    }
 }
 
+function stopTest () {
 
-startButton.addEventListener("click", setTime);
+}
+
+function populateQuestions() {
+    var askQuestions = questions[askQuestionIndex];
+    var titleEl = document.getElementById("question-holder");
+    titleEl.textContent = askQuestions.title;
+
+    choicesEl.innerHTML = "";
+
+    askQuestions.choices.forEach(function(choice, i){
+        var choicesNode = document.createElement("button");
+        choicesNode.setAttribute("class", "choice");
+        choicesNode.setAttribute("value", choice);
+        choicesNode.textContent = i + 1 + "." + choice;
+        choicesNode.onclick = answerChoice;
+        choicesEl.appendChild(choicesNode);
+    });
+
+}
+
+function answerChoice () {
+    if (this.value !== questions[askQuestionIndex].answer) {
+        subtractTime();
+        resultEl.textContent = "Incorrect!";
+    } else {
+        resultEl.textContent = "Correct!";
+    }
+    askQuestionIndex++;
+    if (askQuestionIndex === questions.length) {
+        //stopTest();
+    } else {
+        populateQuestions();
+    }
+
+}
+
+// function Quizstart () {
+//     startEl.setAttribute("class", "hide");
+//     quizEl.setAttribute("class", "show");
+//     setTime();
+//     populateQuestions();
+// }
+
+
+startButton.addEventListener("click", function Quizstart () {
+    startButton.setAttribute("class", "hide")
+    quizEl.setAttribute("class", "show");
+    setTime();
+    populateQuestions();
+});
